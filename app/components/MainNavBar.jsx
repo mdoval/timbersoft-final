dd7bafa1ef2cb1946f60343f0a7587fed626999d
Martin Doval <martindoval@gmail.com>
2023-11-09 19:14:56 +0100

    MainNavBar

--- app/components/MainNavBar.jsx
+++ app/components/MainNavBar.jsx
@@ -6,17 +6,13 @@ export async function MainNavBar() {
   const { user } = await getServerSession(authOptions);
 
   return (
-    <ul className="w-full bg-red-200 flex p-3">
+    <ul className="w-full bg-gray-700 text-white flex p-3">
       <li className="m-3"><Link href="/dashboard/rollos">Rollos</Link></li>
       <li className="m-3">Categorias</li>
       <li className="m-3">Destinos</li>
-      <li>
-        <div className="w-full bg-blue-200">
-          <img
-            className="inline-block h-12 w-12 rounded-full ring-2 ring-white"
-            src={user.image}
-            alt="{user.handle}"
-          />
+      <li className="w-full">
+        <div className="w-full flex flex-row-reverse">
+          <img className="inline-block h-12 w-12 rounded-full ring-2 ring-white" src={user.image} alt="{user.handle}" />
         </div>
       </li>
     </ul>
