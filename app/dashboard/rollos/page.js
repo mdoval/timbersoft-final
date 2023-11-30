149d1f71d839e90f0202f69782a4a0bb78c93cde
Martin Doval <martindoval@gmail.com>
2023-11-30 19:11:25 +0100

    Rollos Desactivado

--- app/dashboard/rollos/page.js
+++ app/dashboard/rollos/page.js
@@ -1,28 +1,11 @@
 import DataTable from "@/app/components/DataTable";
 
-const columns = ['ID','Fecha', 'Nro. de Remito', 'Toneladas','Unidades', 'Flete', 'Largo', 'Proveedor', 'Precio', 'Importe', 'Transportista', 'Factura', 'Categoria', 'Destino' ];
-
-async function getData() {
-  const res = await fetch("http://localhost:3000/api/rollos");
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
-export default async function Page() {
-  const rollos = await getData();
+export default async function RollosPage() {
 
   return (
     <div className="bg-white w-full h-5/6 shadow-lg p-4 rounded-lg overflow-auto">
       <h1>Tabla de Rollos</h1>
-      <hr></hr>
-      <DataTable columns={columns} data={rollos} />
+      <hr>Rollos</hr>
     </div>
   );
 }
