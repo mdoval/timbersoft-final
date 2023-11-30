559c401a16d2341d6fab16b660754ecc31f8ee44
Martin Doval <martindoval@gmail.com>
2023-11-30 19:08:47 +0100

    Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware"
 
 export const config = {
-  matcher: ["/dashboard/:path*","/api/proveedores","/api/categorias","/api/destinos"],
+  matcher: ["/dashboard/:path*","/api/proveedores","/api/categorias","/api/destinos","/api/calidades","/api/destinos","/api/largos","/api/proveedores","/api/transportistas"],
 }
