c155736fca6d8952031002cd06b02196424b0d49
Martin Doval <martindoval@gmail.com>
2024-01-31 18:58:46 +0100

    Fondo pagina de inicio

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import LoginForm from "./components/Login/LoginForm";
 export default function Home() {
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="hero min-h-screen bg-base-200" style={{backgroundImage: 'url(http://localhost:3000/img/background.jpg)'}}>
+        <div className="hero-content flex-col lg:flex-row-reverse bg-amber-800 shadow-lg rounded-xl">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+            <h1 className="text-5xl font-bold text-white">TimberSofrt!</h1>
+            <p className="py-6 text-white">
+              Ingresa ahora al sistema de gestion de aserraderos
+              todos tus datos en un solo lugar
+              Sistema de gestion.
             </p>
           </div>
           <div className="card shrink-0 shadow-2xl bg-base-100 w-1/2">
