1c89a806203d9b4955bea28290abdc4cf39d3fea
Martin Doval <martindoval@gmail.com>
2024-02-23 19:23:52 +0100

    paquetes add

--- app/api/paquetes/add/route.tsx
+++ app/api/paquetes/add/route.tsx
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/utils/authOptions";
 import { getAserraderoId } from "@/utils/getAserraderoId";
-import { getSession } from "next-auth/react";
 
 export async function POST(req: NextRequest ) {
     const session = await getServerSession(authOptions); 
