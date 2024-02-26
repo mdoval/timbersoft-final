c2d058ea2f29f14083e46d355ce6af1b4abee0bc
Martin Doval <martindoval@gmail.com>
2024-02-26 19:49:06 +0100

    paquetes-add

--- app/components/Forms/PaqueteFormMobile.tsx
+++ app/components/Forms/PaqueteFormMobile.tsx
@@ -10,15 +10,15 @@ interface PaqueteFormProps {
   tipos: ITipo[];
 }
 
-const PaqueteFormMobile: FC<PaqueteFormProps> = ({calidades, tipos}) => {
-  const router = useRouter()
+const PaqueteFormMobile: FC<PaqueteFormProps> = ({ calidades, tipos }) => {
+  const router = useRouter();
   const [paquete, setPaquete] = useState<IPaquete>({
     espesor: 0,
     largo: 0,
     ancho: 0,
     cantidad: 0,
     calidad: undefined,
-    tipo: undefined
+    tipo: undefined,
   });
   const [calidad, setCalidad] = useState<ICalidad>(calidades[0]);
   const [tipo, setTipo] = useState<ITipo>(tipos[0]);
@@ -26,23 +26,31 @@ const PaqueteFormMobile: FC<PaqueteFormProps> = ({calidades, tipos}) => {
   const inputAnchoRef = useRef<HTMLInputElement>(null);
   const inputLargoRef = useRef<HTMLInputElement>(null);
   const inputCantidadRef = useRef<HTMLInputElement>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const handleClickEspesor = () => {
-    if (inputEspesorRef.current) { inputEspesorRef.current.select();}    
+    if (inputEspesorRef.current) {
+      inputEspesorRef.current.select();
+    }
   };
 
   const handleClickAncho = () => {
-    if (inputAnchoRef.current) { inputAnchoRef.current.select();}    
+    if (inputAnchoRef.current) {
+      inputAnchoRef.current.select();
+    }
   };
 
   const handleClickLargo = () => {
-    if (inputLargoRef.current) { inputLargoRef.current.select();}    
+    if (inputLargoRef.current) {
+      inputLargoRef.current.select();
+    }
   };
 
   const handleClickCantidad = () => {
-    if (inputCantidadRef.current) { inputCantidadRef.current.select();}    
+    if (inputCantidadRef.current) {
+      inputCantidadRef.current.select();
+    }
   };
-
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const propertyName = e.target.name;
@@ -60,30 +68,46 @@ const PaqueteFormMobile: FC<PaqueteFormProps> = ({calidades, tipos}) => {
   };
 
   const handleCargarPaquete = async () => {
+    setLoading(true);
     const newPaquete: IPaquete = {
       espesor: paquete.espesor,
       ancho: paquete.ancho,
       largo: paquete.largo,
-      cantidad: paquete.cantidad,      
+      cantidad: paquete.cantidad,
       calidadId: calidad.id,
       tipoId: tipo.id,
-      estadoId: 1
+      estadoId: 1,
+    };
+    try {
+      const remitoGuardado = await addPaquete(newPaquete);
+      setPaquete({
+        espesor: 0,
+        largo: 0,
+        ancho: 0,
+        cantidad: 0,
+        calidad: undefined,
+        tipo: undefined,
+      });
+      router.refresh();
+      router.push("/mobile/cargapaquetes/cargar");
+    } catch (error) {
+      console.log(error);
     }
-    const remitoGuardado = await addPaquete(newPaquete);
-    router.push("/mobile/cargapaquetes/cargar");
-    router.refresh();
-
-  }
+  };
 
   const handleCalidadChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const tipoEncontrado = calidades.find(calidad => calidad.id === Number(e.target.value)) as ICalidad
-    setCalidad(tipoEncontrado)
-  }
+    const tipoEncontrado = calidades.find(
+      (calidad) => calidad.id === Number(e.target.value)
+    ) as ICalidad;
+    setCalidad(tipoEncontrado);
+  };
 
   const handleTipoChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const tipoEncontrado = tipos.find(tipo => tipo.id === Number(e.target.value)) as ITipo
-    setTipo(tipoEncontrado)
-  }
+    const tipoEncontrado = tipos.find(
+      (tipo) => tipo.id === Number(e.target.value)
+    ) as ITipo;
+    setTipo(tipoEncontrado);
+  };
 
   return (
     <div className="w-full flex flex-col items-center space-y-4">
@@ -160,18 +184,18 @@ const PaqueteFormMobile: FC<PaqueteFormProps> = ({calidades, tipos}) => {
           <span className="label-text text-1xl font-bold">Calidad</span>
         </div>
         <select
-              className="select select-bordered"
-              defaultValue={calidad.id}
-              onChange={handleCalidadChange}
-            >
-              {calidades.map((calidad) => {
-                return (
-                  <option key={calidad.id} value={calidad.id}>
-                    {calidad.nombre}
-                  </option>
-                );
-              })}
-            </select>
+          className="select select-bordered"
+          defaultValue={calidad.id}
+          onChange={handleCalidadChange}
+        >
+          {calidades.map((calidad) => {
+            return (
+              <option key={calidad.id} value={calidad.id}>
+                {calidad.nombre}
+              </option>
+            );
+          })}
+        </select>
       </label>
 
       <label className="form-control w-full max-w-xs">
@@ -179,23 +203,37 @@ const PaqueteFormMobile: FC<PaqueteFormProps> = ({calidades, tipos}) => {
           <span className="label-text text-1xl font-bold">Tipos</span>
         </div>
         <select
-              className="select select-bordered"
-              defaultValue={tipo.id}
-              onChange={handleTipoChange}
-            >
-              {tipos.map((tipo) => {
-                return (
-                  <option key={tipo.id} value={tipo.id}>
-                    {tipo.catalogo}
-                  </option>
-                );
-              })}
-            </select>
+          className="select select-bordered"
+          defaultValue={tipo.id}
+          onChange={handleTipoChange}
+        >
+          {tipos.map((tipo) => {
+            return (
+              <option key={tipo.id} value={tipo.id}>
+                {tipo.catalogo}
+              </option>
+            );
+          })}
+        </select>
       </label>
 
       <div>
-        <button className="btn btn-wide btn-primary m-2" onClick={handleCargarPaquete}>Guardar</button>
+        <button
+          className="btn btn-wide btn-primary m-2"
+          onClick={handleCargarPaquete}
+        >
+          Guardar
+        </button>
       </div>
+
+      <dialog
+        id="my_modal_1"
+        className={`modal ${loading ? "" : "modal-open"}`}
+      >
+        <div className="modal-box text-center">
+          <span className="loading loading-spinner loading-lg mt-10 mb-10"></span>
+        </div>
+      </dialog>
     </div>
   );
 };
