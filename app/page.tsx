106f77a044928279c416d31c38028e5e0ce7c4fe
Martinchoe <martindoval@gmail.com>
2024-03-07 21:32:03 -0300

    background corregido

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import BotonLogin from "@/app/components/BotonLogin";
 import LoginForm from "./components/Login/LoginForm";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="hero min-h-screen bg-base-200" style={{backgroundImage: 'url(http://localhost:3000/img/background.jpg)'}}>
+      <div className="hero min-h-screen bg-base-200" style={{backgroundImage: `url(${process.env.siteUrl}/img/background.jpg)`}}>
         <div className="hero-content flex-col lg:flex-row-reverse bg-amber-800 shadow-lg rounded-xl">
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold text-white">TimberSofrt!</h1>
